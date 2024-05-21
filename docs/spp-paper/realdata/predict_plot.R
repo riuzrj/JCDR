12eb497e917616a7a4a4e79743c06b8d13c5d505
riuzrj <167947098+riuzrj@users.noreply.github.com>
2024-05-21 10:32:42 +0800

    Update predict_plot.R

--- docs/spp-paper/realdata/predict_plot.R
+++ docs/spp-paper/realdata/predict_plot.R
@@ -7,9 +7,13 @@ library(datamicroarray)
 library(dplyr)
 library(smotefamily)
 
-file_source <- list.files('/Users/ruijuanzhong/spp')
+file_source <- list.files('/Users/ruijuanzhong/spp/R')
 for (i in 1:length(file_source)) {
-  file_source[i] <- paste('/Users/ruijuanzhong/spp/',file_source[i],sep ="")}
+  file_source[i] <- paste('/Users/ruijuanzhong/spp/R/',file_source[i],sep ="")}
+sapply(file_source,source)
+file_source <- list.files('/Users/ruijuanzhong/spp/docs/lolR')
+for (i in 1:length(file_source)) {
+  file_source[i] <- paste('/Users/ruijuanzhong/spp/docs/lolR/',file_source[i],sep ="")}
 sapply(file_source,source)
 
 no_cores = detectCores()
